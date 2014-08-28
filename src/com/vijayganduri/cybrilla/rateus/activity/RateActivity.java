1405bc962956e3f355bba867dad8f62e8d09d483
Vijay Ganduri <vijayganduri@gmail.com>
2014-08-28 08:28:57 +0530

    setup screens

---
+++ src/com/vijayganduri/cybrilla/rateus/activity/RateActivity.java
@@ -0,0 +1,24 @@
+package com.vijayganduri.cybrilla.rateus.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+import com.vijayganduri.cybrilla.rateus.R;
+
+public class RateActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_rate);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.rate, menu);
+		return true;
+	}
+
+}
