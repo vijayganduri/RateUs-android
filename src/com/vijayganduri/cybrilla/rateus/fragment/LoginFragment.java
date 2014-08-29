348dc79885409a75aadedda20936343148875052
Vijay Ganduri <vijay.cellapp@exaget.com>
2014-08-29 17:26:45 +0530

    update google plus library

--- src/com/vijayganduri/cybrilla/rateus/fragment/LoginFragment.java
+++ src/com/vijayganduri/cybrilla/rateus/fragment/LoginFragment.java
@@ -7,16 +7,24 @@ import org.bson.types.ObjectId;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.IntentSender.SendIntentException;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.actionbarsherlock.app.SherlockFragment;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.plus.Plus;
+import com.google.android.gms.plus.model.people.Person;
 import com.vijayganduri.cybrilla.rateus.AppConstants;
 import com.vijayganduri.cybrilla.rateus.MainApplication;
 import com.vijayganduri.cybrilla.rateus.R;
@@ -24,47 +32,90 @@ import com.vijayganduri.cybrilla.rateus.activity.AllServicesActivity;
 import com.vijayganduri.cybrilla.rateus.beans.User;
 import com.vijayganduri.cybrilla.rateus.dao.UserDao;
 
-public class LoginFragment extends SherlockFragment
+/**
+ * Google plus signin based on http://www.androidhive.info/2014/02/android-login-with-google-plus-account-1/
+ * @author Vijay
+ *
+ */
+public class LoginFragment extends SherlockFragment implements ConnectionCallbacks, OnConnectionFailedListener
 {
-	
-	private ImageButton signinBtn;
+
+	private SignInButton signinBtn;
 	private ProgressDialog mDialog;
-	
+
 	private MainApplication app;
 	private UserDao userDao;
 
+	private static final int RC_SIGN_IN = 0;
+	private GoogleApiClient mGoogleApiClient;
+
+	private boolean mIntentInProgress;
+	private boolean mSignInClicked;
+	private ConnectionResult mConnectionResult;
+
 	private static final String TAG = LoginFragment.class.getName();
-	
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-	    return inflater.inflate(R.layout.fragment_login, container, false);
+		return inflater.inflate(R.layout.fragment_login, container, false);
 	}
-	
+
 	@Override
 	public void onViewCreated(View view, Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
-		
+
 		app = (MainApplication)getActivity().getApplication();
 		userDao = app.getUserDao();
-		
-		signinBtn = (ImageButton) view.findViewById(R.id.signinBtn);
+
+		signinBtn = (SignInButton) view.findViewById(R.id.signinBtn);
 		signinBtn.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				startSignin();
 			}
-		});
-	}
-	
-	private void startSignin(){
-		//TODO setup google + auth		
-		new RegisterUserAsyncTask().execute();
-	}
-	
+		});		
 
-	private class RegisterUserAsyncTask extends AsyncTask<Void, Void, User>{
+		mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
+		.addConnectionCallbacks(this)
+		.addOnConnectionFailedListener(this).addApi(Plus.API, null)
+		.addScope(Plus.SCOPE_PLUS_LOGIN).build();
+	}	
+
+	public void onStart() {
+		super.onStart();
+		mGoogleApiClient.connect();
+	}
+
+	public void onStop() {
+		super.onStop();
+		if (mGoogleApiClient.isConnected()) {
+			mGoogleApiClient.disconnect();
+		}
+	}
+
+	private void startSignin(){
+		showDialog(getString(R.string.info_signing));
+		if (!mGoogleApiClient.isConnecting()) {
+			mSignInClicked = true;
+			resolveSignInError();
+		}
+	}
+
+	private void resolveSignInError() {
+		if (mConnectionResult.hasResolution()) {
+			try {
+				mIntentInProgress = true;
+				mConnectionResult.startResolutionForResult(getActivity(), RC_SIGN_IN);
+			} catch (SendIntentException e) {
+				mIntentInProgress = false;
+				mGoogleApiClient.connect();
+			}
+		}
+	}
+
+	private class RegisterUserAsyncTask extends AsyncTask<String, Void, User>{
 
 		@Override
 		protected void onPreExecute() {
@@ -73,9 +124,10 @@ public class LoginFragment extends SherlockFragment
 		}
 
 		@Override
-		protected User doInBackground(Void... params) {
+		protected User doInBackground(String... params) {
+			
 			//After successful Google + signin need to save it to db			
-			return onLoginSuccess();
+			return onLoginSuccess(params[0], params[1]);
 		}
 
 		@Override
@@ -85,20 +137,20 @@ public class LoginFragment extends SherlockFragment
 		}
 
 	}
-	
+
 	/*
 	 * Will be invoked after Google plus signin is successful
 	 * Store the user to internal db
 	 */
-	private User onLoginSuccess(){
-		
+	private User onLoginSuccess(String name, String email){
+
 		User user = new User();
-		user.setEmail("xyz");//TODO Fill with exact content
-		user.setName("abc");
+		user.setEmail(email);
+		user.setName(name);
 		String id = new ObjectId().toStringMongod();
 		BigInteger idValue = new BigInteger(id, 16);
 		user.setId(idValue.longValue());
-		
+
 		try {
 			user = userDao.createIfNotExists(user);
 		} catch (SQLException e) {
@@ -108,11 +160,11 @@ public class LoginFragment extends SherlockFragment
 		}
 		return user;
 	}
-	
+
 	private void onLoginFailed(){
 		showToast(getString(R.string.warn_login_fail));
 	}	
-	
+
 	/*
 	 * Start the AllServices screen for user to vote.
 	 * User should rate immediately. On pressing back user session gets terminated and needs to relogin
@@ -127,7 +179,7 @@ public class LoginFragment extends SherlockFragment
 	private void showToast(CharSequence msg){
 		Toast.makeText(getActivity(), msg, Toast.LENGTH_LONG).show();
 	}
-	
+
 	private void showDialog(CharSequence msg){
 		cancelDialog();
 		mDialog = new ProgressDialog(getActivity());
@@ -135,11 +187,61 @@ public class LoginFragment extends SherlockFragment
 		mDialog.setCancelable(true);
 		mDialog.show();
 	}
-	
+
 	private void cancelDialog(){
 		if(mDialog!=null && mDialog.isShowing()){
 			mDialog.cancel();
 		}
 	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult result) {
+		if (!result.hasResolution()) {
+			GooglePlayServicesUtil.getErrorDialog(result.getErrorCode(), getActivity(),
+					0).show();
+			return;
+		}
+
+		if (!mIntentInProgress) {
+			mConnectionResult = result;
+
+			if (mSignInClicked) {
+				resolveSignInError();
+			}
+		}
+	}
+
+	@Override
+	public void onConnected(Bundle arg0) {
+		mSignInClicked = false;
+		getProfileInformation();
+	}
 	
+	/**
+	 * Fetching user's information name, email
+	 * */
+	private void getProfileInformation() {
+		try {
+			if (Plus.PeopleApi.getCurrentPerson(mGoogleApiClient) != null) {
+				Person currentPerson = Plus.PeopleApi
+						.getCurrentPerson(mGoogleApiClient);
+				String personName = currentPerson.getDisplayName();
+				String email = Plus.AccountApi.getAccountName(mGoogleApiClient);
+
+				new RegisterUserAsyncTask().execute(personName, email);
+				return;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		// proceed for saving in internal db
+		new RegisterUserAsyncTask().execute();
+	}
+
+	@Override
+	public void onConnectionSuspended(int arg0) {
+		mGoogleApiClient.connect();
+		Toast.makeText(getActivity(), "User login failed", Toast.LENGTH_LONG).show();
+		cancelDialog();
+	}
 }
